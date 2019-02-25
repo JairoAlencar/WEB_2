e7ddba2773fba3b5ba6c345558903e0e3151f241
1831216 <1831216@LABBMAQ11.itp.local>
2019-02-25 10:19:44 -0300

    Finalizado 1

--- Lista_2.php
+++ Lista_2.php
@@ -1,3 +1,80 @@
+//Exercicio 1
+<?php 
+
+$i=0;
+$vetor = array();
+
+    for($i=0;$i<51;$i++){
+        $vetor[$i]=42;
+         
+    }
+    echo "<pre>";
+    print_r ($vetor);
+    echo "</pre>";
+
+
+//Exercico 2
+	
+	<?php 
+
+$i=0;
+$vetor = array();
+
+    for($i=0;$i<51;$i++){
+        $vetor[$i]=$i;
+         
+    }
+    echo "<pre>";
+    print_r ($vetor);
+    echo "</pre>";
+
+
+//Exercicio 3
+	<?php 
+
+$media=0;
+$soma=0;
+$vetor = array();
+
+    for($i=0;$i<30;$i++){
+        $vetor[$i]=$i+1;
+        $soma=$soma+$i;    
+    }
+    $resultado=array();
+        for($i=1; $i<30; $i++){
+            if($vetor[$i]>$vetor[$i-1]){
+                $resultado[0]=$vetor[$i];
+                $resultado[1]=$i;
+                $media+=1;
+            }
+        }
+    
+    echo "<pre>";
+    print_r ($vetor);
+    echo "</pre>";
+    
+print "Maior valor:".$i."<br>";
+print "Que esta na posição:".$resultado[1];
+print "<br>"."A soma de todos esse valores é:".$soma."<br>";
+print "A media é de:".$soma/$media;
+
+
+
+//Exercicio 4
+	<?php 
+
+$i=0;
+$vetor = array();
+
+    for($i=0;$i<51;$i++){
+        $vetor[$i]=$i*2;
+         
+    }
+    echo "<pre>";
+    print_r ($vetor);
+    echo "</pre>";
+
+//Exercicio 5
 
 <select>
     <?php
