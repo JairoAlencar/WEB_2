83ce6adf056b8d4cc0c25b4e870a5e48974eec25
1831216 <1831216@BIBLIOMAQ10.itp.local>
2019-02-26 15:51:43 -0300

    Melhoramento da divisão

---
+++ Lista_3.php
@@ -0,0 +1,34 @@
+<?php print "Exercicio 1"; ?>
+<?php
+
+
+
+?>
+
+<?php print "Exercicio 2"; ?>
+<?php
+
+
+
+?> 
+
+<?php print "Exercicio 3"; ?>
+<?php
+
+
+
+?>
+
+<?php print "Exercicio 4"; ?>
+<?php
+
+
+
+?>
+
+<?php print "Exercicio 5"; ?>
+<?php
+
+
+
+?>
