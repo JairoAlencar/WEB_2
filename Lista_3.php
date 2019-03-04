02bf92326a574927bea5085d459ffff733214804
JAIRO <JAIRO@192.168.0.4>
2019-03-04 15:00:52 -0300

    Finalizada

--- Lista_3.php
+++ Lista_3.php
@@ -1,22 +1,55 @@
 <?php print "Exercicio 1"."<br>"; ?>
 <?php
-   echo"teste1"; 
-
-
+    function primo(){
+        $cont=0;
+        $num=1;
+            for($i=0; $i<=$num; $i++){
+                if($num%$i==0){
+                    $cont++;
+                }
+            }
+            if($cont==2){
+                print"Esse numero é primo"."<br>";
+            }else{
+                print"Esse numero não é primo"."<br>";
+            }
+            
+    }
+    primo();
 ?>
 
 <?php print"<br>";
 print "Exercicio 2"."<br>"; ?>
 <?php
 
+$num = range(0,100);
 
-
+    foreach($num AS $i => $a){
+        if($a%2!=0) {
+             echo "$a ";
+        }	
+    }
+print"<br>";
 ?> 
 
 <?php print"<br>";
 print "Exercicio 3"."<br>"; ?>
-<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<ul>
+		<?php $cores=array('Branco', 'Rosa', 'Laranja'); ?>
+		 <li><?php print_r($cores[0]) ?></li>
+		 <li><?php print_r($cores[1]) ?></li>
+                 <li><?php print_r($cores[2]) ?></li>
+        </ul>
+</body>
+</html>
+    
 
 
 
-?>
