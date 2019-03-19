672317e2fec9f5ae13b9389d696542a373bd6d5e
JAIRO <JAIRO@192.168.0.4>
2019-03-18 21:31:48 -0300

    formulario atualizado

--- Lista4/face.php
+++ Lista4/face.php
@@ -5,12 +5,12 @@ $sobre = $_POST["sobrenome_user"];
 $email = $_POST["email"];
 $confemail = $_POST["confemail"];
 $senha = $_POST["senha"];
-$aniversario = $_POST["dia"&&"mes"&&"ano"];
+$aniversario = $_POST["dia"]."/".$_POST["mes"]."/".$_POST["ano"];
 $sexo = $_POST["sexo"];  
 
 if($email==$confemail){
-    print"Seja Bem-Vindo ".$nome." ".$sobre."<br>";
-    print"Enviaremos uma confirmação da sua conta para o email ".$email."<br>";
+    print"Seja Bem-Vindo ".$nome." ".$sobre.".<br>";
+    print"Enviaremos uma confirmação do seu cadastro para o email ".$email."<br>";
 }else{
-    print"Os e-mails não se batem";
+    print"Os e-mails não batem";
 }
